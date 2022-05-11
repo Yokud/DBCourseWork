96de848571590828654e0f6af77530778268f83a
Malyshev Ivan <thefuny@mail.ru>
2022-05-11 21:22:40 +0300

    some changes

---
+++ src/proj/DBCourseWork/DataBaseUI/Models/PgSQLSaleReceiptPositionsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseUI.Models
+{
+    internal class PgSQLSaleReceiptPositionsRepository : IRepository<SaleReceiptPosition>
+    {
+        public void Create(SaleReceiptPosition item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SaleReceiptPosition Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SaleReceiptPosition> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(SaleReceiptPosition item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/proj/DBCourseWork/DataBaseUI/Models/PgSQLSaleReceiptsRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBaseUI.Models
+{
+    internal class PgSQLSaleReceiptsRepository : IRepository<SaleReceipt>
+    {
+        public void Create(SaleReceipt item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SaleReceipt Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SaleReceipt> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(SaleReceipt item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
