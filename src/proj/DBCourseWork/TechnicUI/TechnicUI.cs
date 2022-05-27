ef54af2ac0c9504ded88118cb033611cae439014
Malyshev Ivan <thefuny@mail.ru>
2022-05-27 14:58:05 +0300

    Update TechnicUI.cs

--- src/proj/DBCourseWork/TechnicUI/TechnicUI.cs
+++ src/proj/DBCourseWork/TechnicUI/TechnicUI.cs
@@ -16,6 +16,19 @@ namespace TechnicUI
         ICostsRepository costs;
         ICostStoryRepository costStory;
 
+        string menuString = "Технический интерфейс работы с БД с доступом ко всем командам.\nМеню:\n" +
+            "\t1)  Получить магазин по Id;\n" +
+            "\t2)  Получить все магазины;\n" +
+            "\t3)  Добавить магазин;\n" +
+            "\t4)  Удалить магазин;\n" +
+            "\t5)  Изменить данные о магазине;\n\n\n" +
+            "\t6)  Получить продукт по Id;\n" +
+            "\t7)  Получить все продукты;\n" +
+            "\t8)  Добавить продукт;\n" +
+            "\t9)  Удалить продукт;\n" +
+            "\t10) " +
+            "\t0) Выход.\n\nВведите номер команды: ";
+
         public TechnicUI(IShopsRepository shops, IProductsRepository products, ISaleReceiptsRepository saleReceipts, ISaleReceiptPositionsRepository saleReceiptPositions, ICostsRepository costs, ICostStoryRepository costStory)
         {
             this.shops = shops;
@@ -26,6 +39,14 @@ namespace TechnicUI
             this.costStory = costStory;
         }
 
+        public void StartUi()
+        {
+            bool isFinished = false;
 
+            while (!isFinished)
+            {
+
+            }
+        }
     }
 }
